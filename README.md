44c9eb63419736edd43b7b15b122928e834a6adb
Gerry Manoim <gmanoim@quantopian.com>
2020-06-25 20:41:29 -0400

    Cmake builds

--- README.md
+++ README.md
@@ -15,10 +15,6 @@ Heavily inspired by https://github.com/tinysearch/tinysearch.
 Not ideal. Just for my notes:
 
 ```
-cd src
-g++-9 --std=gnu++17 -I/usr/local/Cellar/cereal/1.3.0/include -I/usr/local/Cellar/nlohmann-json/3.7.3/include -I/usr/local/Cellar/boost/1.72.0_3/include main.cc -o main
-main < ../fixtures/index.json
-cp out.bin web/post_data.bin
 cd web
 em++ --std=gnu++17 -I/usr/local/Cellar/cereal/1.3.0/include -I/usr/local/Cellar/nlohmann-json/3.7.3/include -I/usr/local/Cellar/boost/1.72.0_3/include --bind --preload-file post_data.bin -o static_search.js main.cc
 python -m http.server 8999
@@ -26,6 +22,8 @@ python -m http.server 8999
 
 
 ## TODO
+
+- [ ] End to end program run
 
 - [ ] Better dependency inclusion
 - [ ] Makefile
--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,12 @@
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 
+// for the life of me I cannot get boost+macOS+gcc-9 to play nicely so using this instead
+#include <cxxopts.hpp>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/tokenizer.hpp>
@@ -17,27 +20,27 @@
 
 using name_url = std::pair<std::string, std::string>;
 
-struct PairHash
-{
+struct PairHash {
     // TODO would it be better to just compbine the strings?
-    std::size_t operator()(const name_url &k) const
-    {
-        return std::hash<std::string>()(k.first) ^ (std::hash<std::string>()(k.second) << 1);
+    std::size_t operator()(const name_url& k) const {
+        return std::hash<std::string>()(k.first) ^
+               (std::hash<std::string>()(k.second) << 1);
     }
 };
 
 // for convenience
 using json = nlohmann::json;
 
-int main()
-{
+void process_input(const std::filesystem::path& input_file_path,
+                   const std::filesystem::path& output_directory) {
+    std::cout << "Parsing json at " << input_file_path;
     json j;
     using post_contents = std::unordered_set<std::string>;
     std::unordered_map<name_url, post_contents, PairHash> post_map;
+    std::ifstream i(input_file_path);
+    i >> j;
 
-    std::cin >> j;
-    for (auto &post : j)
-    {
+    for (auto& post : j) {
         std::cout << "Processing " << post.at("title") << "\n";
         name_url key(post.at("title"), post.at("url"));
         std::string post_text = post.at("body");
@@ -47,9 +50,34 @@ int main()
         post_map[key] = post_contents(tokens.begin(), tokens.end());
     };
 
+    auto build_dir = std::filesystem::temp_directory_path() / "static_searcher";
+    std::filesystem::create_directory(build_dir);
+    std::cout << "Building inside of " << build_dir;
     {
-        std::ofstream output("./out.bin", std::ios::binary);
+        std::ofstream output(build_dir / "post_data.bin", std::ios::binary);
         cereal::BinaryOutputArchive archive(output);
         archive(post_map);
     }
+}
+
+int main(int argc, char** argv) {
+
+    // clang-format off
+    cxxopts::Options options("StaticSearcher", "WebAssembly Static Site Searcher");
+    options.add_options()
+        ("help", "Print usage")
+        ("i,input", "input file path", cxxopts::value<std::string>())
+        ("output", "output directory", cxxopts::value<std::string>())
+    ;
+    // clang-format on
+
+    auto passed_options = options.parse(argc, argv);
+
+    if (passed_options.count("help") or !passed_options.count("input")) {
+        std::cout << options.help() << '\n';
+        return 1;
+    }
+
+    process_input(passed_options["input"].as<std::string>(),
+                  passed_options["output"].as<std::string>());
 }
